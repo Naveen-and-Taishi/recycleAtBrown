63f855c84d2a6854821b46c16edb8ec007a799ab
Taishi Nishizawa <taishi.nishizawa201@gmail.com>
2022-01-29 23:37:06 -0500

    still trying to get it to work

--- deep_learning/code/model.py
+++ deep_learning/code/model.py
@@ -6,21 +6,24 @@ import math
 from matplotlib import pyplot as plt
 from preprocessing import get_data
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Reshape, Dropout
 from tensorflow.math import exp, sqrt, square
 from tensorflow.keras.applications import ResNet50V2
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Reshape, MaxPooling2D, Dropout, Conv2D, GlobalAveragePooling2D, UpSampling2D
+from tensorflow.keras.layers.experimental.preprocessing import RandomFlip, RandomRotation
 
 class Referee(tf.keras.Model):
-    def __init__(self, batch_size) -> None:
+    def __init__(self) -> None:
         super(Referee, self).__init__()
 
         self.num_classes = 6
         self.learning_rate = 0.00001
         self.decay_rate = 0.00000025
-        self.dropout_rate = 0.1
+        self.dropout_rate = 0.01
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
-        self.batch_size = batch_size
-        self.hidden_size = 128
+        self.batch_size = 100
+        self.hidden_size = 500
+        self.conv_size = 128 
 
         self.ResNet50 = ResNet50V2(include_top=False, 
             weights="imagenet",
@@ -36,7 +39,19 @@ class Referee(tf.keras.Model):
             Dropout(self.dropout_rate),
             Dense(6, activation="relu")
         ])
-        pass
+
+        # self.referee = Sequential([
+        #     Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'),
+        #     MaxPooling2D(pool_size=(2, 2)),
+        #     Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'),
+        #     MaxPooling2D(pool_size=(2, 2)),
+        #     Conv2D(self.conv_size, kernel_size=(3, 3), activation='relu'),
+        #     MaxPooling2D(pool_size=(2, 2)),
+        #     Flatten(),
+        #     Dense(self.hidden_size, activation='relu'),
+        #     Dense(self.hidden_size, activation='relu'),
+        #     Dense(6, activation='softmax')])
+
 
     def call(self, inputs):
         # outputs a 1 by 1 by 20
@@ -59,18 +74,26 @@ class Referee(tf.keras.Model):
 
         labels should be one-hot vector
         """
+        print("logits", logits)
+        # print("labels Shape", labels.shape)
         labelsOneHot = tf.one_hot(labels, self.num_classes)
         correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labelsOneHot, 1))
-        print(correct_predictions) # correct predictions should be a one-dimensional list of labels
+        #print(correct_predictions) # correct predictions should be a one-dimensional list of labels
+        # print("dimension1: ", tf.argmax(logits, 1))
+        # print("dimension1: ", tf.argmax(labelsOneHot, 1))
+        # print("dimension2", tf.argmax(logits, 0))
+        # print("dimension2", tf.argmax(labelsOneHot, 0))
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
 def train(model, train_inputs, train_labels):
-    for epoch in range(20):
+    for epoch in range(5):
         print("EPOCH: " + str(epoch))
         for b in range(0, len(train_inputs), model.batch_size):
             print("batch: " + str(int(b / model.batch_size)))
-            batch_inputs = tf.convert_to_tensor(train_inputs[b : b + model.batch_size])
-            batch_labels = tf.convert_to_tensor(train_labels[b : b + model.batch_size])
+            batch_inputs = tf.convert_to_tensor(train_inputs[b : b + model.batch_size], dtype=tf.int32)
+            batch_labels = tf.convert_to_tensor(train_labels[b : b + model.batch_size], dtype=tf.int32)
+            # batch_inputs = tf.convert_to_tensor(train_inputs[b : b + model.batch_size])
+            # batch_labels = tf.convert_to_tensor(train_labels[b : b + model.batch_size])
             with tf.GradientTape() as tape:
                 logits = model.call(batch_inputs)
                 loss = model.loss(logits, batch_labels)
@@ -87,8 +110,10 @@ def test(model, test_inputs, test_labels):
     num_examples = model.batch_size 
     for i in range(0, len(test_inputs), num_examples): 
         # print(i)
-        images = tf.convert_to_tensor(test_inputs[i:i + num_examples])
-        labels = tf.convert_to_tensor(test_labels[i:i + num_examples])
+        images = tf.convert_to_tensor(test_inputs[i:i + num_examples], dtype=tf.int32)
+        labels = tf.convert_to_tensor(test_labels[i:i + num_examples], dtype=tf.int32)
+        # images = tf.convert_to_tensor(test_inputs[i:i + num_examples])
+        # labels = tf.convert_to_tensor(test_labels[i:i + num_examples])
         # print(test_inputs[i].shape)
         predictions = model.call(images)
         loss = model.loss(predictions, labels)
@@ -101,9 +126,7 @@ def test(model, test_inputs, test_labels):
 def main():
     training_images, training_labels, testing_images, testing_labels = get_data("../data")
 
-    batch_size = 100
-
-    referee = Referee(batch_size)
+    referee = Referee()
 
     print("Starting training...")
     train(referee, training_images, training_labels)
@@ -112,5 +135,6 @@ def main():
 
     print("FINAL TESTING ACCURACY: " + str(final_acc))
 
+    tf.save_model.save(referee, "../models")
 if __name__ == '__main__':
     main()
