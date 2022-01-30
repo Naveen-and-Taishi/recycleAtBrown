7908c99865165daa8bfd4d97afaf5e6df164677c
Taishi Nishizawa <taishi.nishizawa201@gmail.com>
2022-01-29 20:00:03 -0500

    started react, got model to work w/ low accuracy

--- deep_learning/code/model.py
+++ deep_learning/code/model.py
@@ -14,14 +14,13 @@ class Referee(tf.keras.Model):
     def __init__(self, batch_size) -> None:
         super(Referee, self).__init__()
 
-        # TODO: Initialize all hyperparameters
+        self.num_classes = 6
         self.learning_rate = 0.00001
         self.decay_rate = 0.00000025
-        self.Adam = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
         self.batch_size = batch_size
         self.hidden_size = 128
 
-        # TODO: Initialize all trainable parameters
 
         self.ResNet50 = ResNet50V2(include_top=False, 
             weights="imagenet",
@@ -38,7 +37,6 @@ class Referee(tf.keras.Model):
         pass
 
     def call(self, inputs):
-        # TODO: Write forward-pass logic
         # outputs a 1 by 1 by 20
         resnet_output = tf.stop_gradient(self.ResNet50(inputs))
         referee_output = self.referee(resnet_output)
@@ -47,7 +45,6 @@ class Referee(tf.keras.Model):
         return referee_output
     
     def loss(self, probs, labels): 
-        # TODO: Write loss function
         loss = tf.keras.losses.sparse_categorical_crossentropy(labels, probs, from_logits=True)
         return tf.reduce_sum(loss)
         
@@ -57,8 +54,12 @@ class Referee(tf.keras.Model):
         containing the result of multiple convolution and feed forward layers
         :param labels: matrix of size (num_labels, self.num_classes) containing the answers, during training, this will be (batch_size, self.num_classes)
         :return: the accuracy of the model as a Tensor
+
+        labels should be one-hot vector
         """
-        correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+        labelsOneHot = tf.one_hot(labels, self.num_classes)
+        correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labelsOneHot, 1))
+        print(correct_predictions) # correct predictions should be a one-dimensional list of labels
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
 def train(model, train_inputs, train_labels):
@@ -66,14 +67,14 @@ def train(model, train_inputs, train_labels):
         print("EPOCH: " + str(epoch))
         for b in range(0, len(train_inputs), model.batch_size):
             print("batch: " + str(int(b / model.batch_size)))
-            batch_inputs = train_inputs[b : b + model.batch_size]
-            batch_labels = train_labels[b : b + model.batch_size]
+            batch_inputs = tf.convert_to_tensor(train_inputs[b : b + model.batch_size])
+            batch_labels = tf.convert_to_tensor(train_labels[b : b + model.batch_size])
             with tf.GradientTape() as tape:
                 logits = model.call(batch_inputs)
                 loss = model.loss(logits, batch_labels)
                 accuracy = model.accuracy(logits, batch_labels)
                 print("ACCURACY: " + str(accuracy))
-            gradients = tape.gradient(loss, model.trainable_variable)
+            gradients = tape.gradient(loss, model.trainable_variables)
             model.optimizer.apply_gradients(zip(gradients, model.trainable_variables)) 
     
 def test(model, test_inputs, test_labels):
@@ -84,8 +85,8 @@ def test(model, test_inputs, test_labels):
     num_examples = model.batch_size 
     for i in range(0, len(test_inputs), num_examples): 
         # print(i)
-        images = test_inputs[i:i + num_examples, :, :]
-        labels = test_labels[i:i + num_examples]
+        images = tf.convert_to_tensor(test_inputs[i:i + num_examples])
+        labels = tf.convert_to_tensor(test_labels[i:i + num_examples])
         # print(test_inputs[i].shape)
         predictions = model.call(images)
         loss = model.loss(predictions, labels)
@@ -98,7 +99,7 @@ def test(model, test_inputs, test_labels):
 def main():
     training_images, training_labels, testing_images, testing_labels = get_data("../data")
 
-    batch_size = 32
+    batch_size = 100
 
     referee = Referee(batch_size)
 
